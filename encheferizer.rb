cd08f3aa7408971d1ce72611654d019e04e61aff
Alex Dean <alex@crackpot.org>
2010-01-17 16:00:24 -0600

    move in_same_case_as into a ChefString proxy object, rather than adding the method directly into String.  Seems better to leave base classes alone as much as possible.  reorganize the code somewhat to make it easier to read.

--- encheferizer.rb
+++ encheferizer.rb
@@ -1,31 +1,6 @@
-class String
-  def in_same_case_as( target )
-    to_case( self, get_case( target ) )
-  end
-  
-  def get_case( letter )
-    code = letter[ 0 ]
-    if code > 96 && code < 123
-      :lower
-    elsif code > 64 && code < 91
-      :upper
-    else
-      :non_alpha
-    end
-  end
-  
-  def to_case( letter, sym )
-    case sym
-    when :upper
-      letter.upcase
-    when :lower
-      letter.downcase
-    else
-      letter
-    end
-  end
-end
-
+# Usage:
+# Cheferize can be included into any class which responds to to_s.  
+# It adds a method to_chef.
 # class String
 #   include Cheferize
 # end
@@ -33,36 +8,19 @@ end
 # >> "This is a test!".to_chef
 # => "Tees is e test!"
 #
-# Cheferize.rules shows you the transliteration rules.
 # These are applied sequentially to each character in the source string until 1 matches.
 module Cheferize
-  
-  class << self
-    def rule( name, &block )
-      @rules ||= []
-      sym = name.to_sym
-      @rules << sym
-      define_method( sym, block )
-    end
-    
-    def rules
-      @rules
-    end
-  end
   
   def to_chef
     # split string into words
     # TODO : punctuation will break rules which assert :last position.
     output = to_s.split( ' ' ).map do |word|
-      cheferize( word )
+      cheferize_word( word )
     end
-    output = output.join( ' ' )
-    
-    # TODO : randomly borkify after . or !
-    
+    output.join( ' ' )
   end
   
-  def cheferize( input )
+  def cheferize_word( input )
     
     i=0
     output = ''
@@ -70,6 +28,7 @@ module Cheferize
     @chef_saw_an_i = nil
     
     while i < input.size do
+      # a single character will be :first but not :last.
       position = nil
       if i == 0
         position = :first
@@ -79,10 +38,12 @@ module Cheferize
       
       # apply rules to each character, breaking on the first one which returns output
       # for the word 'testify', the rules will see 'testify', 'estify', 'stify', 'tify', etc...
+      subject = input[ i..(input.size-1) ]
+      
       Cheferize.rules.each do |rule|
-        subject = input[ (i..input.size) ]
         out = self.send( rule, subject, position )
-        puts "#{subject} : '#{rule.to_s}' returns '#{out.inspect}', #{position.to_s}"
+        # uncomment to see to_chef in action.
+        #puts "#{subject} #{' ('+position.to_s+') ' if position}: '#{rule.to_s}' returns '#{out.inspect}'"
         if out
           output += out[ 0 ]
           i += out[ 1 ]
@@ -97,30 +58,43 @@ module Cheferize
   # General rules
   # after a replacement occurs, skip to the next source character.  (only 1 transformation per letter)
   # if a multi-char test matches, skip over to the next chars. 'few' matches 'ew->oo'.  Then we're done.
-
   
-    # for first char
-      # ^e->i
-      # ^o->oo
-    # for non-first chars
-      # ew->oo
-      # e$->e-a
-      # f->ff
-      # ir->ur, or i->ee if no i has occurred.
-      # ow->oo or o->u
-      # tion -> shun
-      # u->oo
+  # for first char
+    # ^e->i
+    # ^o->oo
+  # for non-first chars
+    # ew->oo
+    # e$->e-a
+    # f->ff
+    # ir->ur, or i->ee if no i has occurred.
+    # ow->oo or o->u
+    # tion -> shun
+    # u->oo
 
-    # An->Un
-    # Au->Oo
-    # A.->E (A not at the end becomes E)
-    # en$->ee
-    # e not at the beginning, do nothing. Pointless?
-    # th->tt
-    # the->zee
-    # v->f
-    # w->v
-    # pass
+  # An->Un
+  # Au->Oo
+  # A.->E (A not at the end becomes E)
+  # en$->ee
+  # e not at the beginning, do nothing. Pointless?
+  # th->tt
+  # the->zee
+  # v->f
+  # w->v
+  # pass
+  
+  # Define a transformation rule.
+  # Block must accept subject & position parameters.
+  def self.rule( name, &block )
+    @rules ||= []
+    sym = name.to_sym
+    @rules << sym
+    define_method( sym, block )
+  end
+
+  # Shows the transliteration rules.
+  def self.rules
+    @rules
+  end
   
   # no recursive cheferizing yet.
   rule "pass the bork" do |subject, position|
@@ -136,14 +110,14 @@ module Cheferize
   # ^e->i
   rule "initial e becomes i" do |subject, position|
     if position == :first && subject[ 0,1 ].downcase == 'e'
-      [ 'i'.in_same_case_as( subject[ 0,1 ] ), 1 ]
+      [ ChefString.new( 'i' ).in_same_case_as( subject[ 0,1 ] ), 1 ]
     end
   end
   
   # ^o->oo
   rule "initial o becomes oo" do |subject, position|
     if position == :first && subject[ 0,1 ].downcase == 'o'
-      [ 'o'.in_same_case_as( subject[ 0,1 ] ) + 'o', 1 ]
+      [ ChefString.new( 'o' ).in_same_case_as( subject[ 0,1 ] ) + 'o', 1 ]
     end
   end
   
@@ -202,21 +176,21 @@ module Cheferize
   # An->Un, an->un
   rule "an becomes un" do |subject, position|
     if subject[ 0,2 ].downcase == 'an'
-      [ 'u'.in_same_case_as( subject[ 0,1 ] ) + 'n', 2 ]
+      [ ChefString.new( 'u' ).in_same_case_as( subject[ 0,1 ] ) + 'n', 2 ]
     end
   end
   
   # Au->Oo, au->oo
   rule "au becomes oo" do |subject, position|
     if subject[ 0,2 ].downcase == 'au'
-      [ 'o'.in_same_case_as( subject[ 0,1 ] ) + 'o', 2 ]
+      [ ChefString.new( 'o' ).in_same_case_as( subject[ 0,1 ] ) + 'o', 2 ]
     end
   end
   
   # A.->E (A not at the end becomes E)
   rule "non-final a becomes e" do |subject, position|
     if position != :last && subject[ 0,1 ].downcase == 'a'
-      [ 'e'.in_same_case_as( subject[ 0,1 ] ), 1 ]
+      [ ChefString.new( 'e' ).in_same_case_as( subject[ 0,1 ] ), 1 ]
     end
   end
   
@@ -232,33 +206,76 @@ module Cheferize
   # the->zee
   rule "the becomes zee" do |subject, position|
     if subject[ 0,3 ].downcase == 'the'
-      [ 'z'.in_same_case_as( subject[ 0,1 ] ) + 'ee', 3 ]
+      [ ChefString.new( 'z' ).in_same_case_as( subject[ 0,1 ] ) + 'ee', 3 ]
     end
   end
   
   # th->t
   rule "th becomes t" do |subject, position|
     if subject[ 0,2 ].downcase == 'th'
-      [ 't'.in_same_case_as( subject[ 0,1 ] ), 2 ]
+      [ ChefString.new( 't' ).in_same_case_as( subject[ 0,1 ] ), 2 ]
     end
   end
   
   # v->f
   rule "v becomes f" do |subject, position|
     if subject[ 0,1 ].downcase == 'v'
-      [ 'f'.in_same_case_as( subject[ 0,1 ] ), 1 ]
+      [ ChefString.new( 'f' ).in_same_case_as( subject[ 0,1 ] ), 1 ]
     end
   end
   
   # w->v
   rule "w becomes v" do |subject, position|
     if subject[ 0,1 ].downcase == 'w'
-      [ 'v'.in_same_case_as( subject[ 0,1 ] ), 1 ]
+      [ ChefString.new( 'v' ).in_same_case_as( subject[ 0,1 ] ), 1 ]
     end
   end
   
   rule "pass on anything else" do |subject, position|
     [ subject[ 0,1 ], 1 ]
+  end
+  
+  # A ChefString provides an additional public method for String.  in_same_case_as.
+  class ChefString
+    instance_methods.each { |m| undef_method m unless m =~ /(^__|^send$|^object_id$)/ }
+
+    def initialize( str )
+      @target = str
+    end
+
+    # return target string, transformed into the same case as the first character of target.
+    def in_same_case_as( target )
+      to_case( @target, get_case( target ) )
+    end
+
+    def get_case( letter )
+      code = letter[ 0 ]
+      if code > 96 && code < 123
+        :lower
+      elsif code > 64 && code < 91
+        :upper
+      else
+        :non_alpha
+      end
+    end
+
+    def to_case( letter, sym )
+      case sym
+      when :upper
+        letter.upcase
+      when :lower
+        letter.downcase
+      else
+        letter
+      end
+    end
+
+    protected
+
+    # proxy any remaining calls to the underlying string.
+    def method_missing(name, *args, &block)
+      @target.send(name, *args, &block)
+    end
   end
   
 end
